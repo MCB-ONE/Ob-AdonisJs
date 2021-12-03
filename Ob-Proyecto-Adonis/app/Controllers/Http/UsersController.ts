7ee9c7ac32938dfa2d4a1a83cb865abb1c8b7d7a
toni <toni.one.dev@gmail.com>
2021-12-04 00:01:37 +0100

    Conectando nuestra base de datos y configurando el controlador. Creando primera migración create_users_table

--- Ob-Proyecto-Adonis/app/Controllers/Http/UsersController.ts
+++ Ob-Proyecto-Adonis/app/Controllers/Http/UsersController.ts
@@ -3,26 +3,25 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 export default class UsersController {
   public async index(ctx: HttpContextContract) {
     return 'GET Users';
-  };
+  }
 
   public async store({ request, response }: HttpContextContract) {
     response.status(201);
     return {
       message: 'POST New User',
       body: request.body(),
-    }
+    };
+  }
 
-  };
+  public async show({ params }: HttpContextContract) {
+    return `GET User: ${params.id}`;
+  }
 
-  public async show({params}:HttpContextContract) {
-    return `GET User: ${params.id}`
-  };
+  public async update({ params }: HttpContextContract) {
+    return `PUT User: ${params.id}`;
+  }
 
-  public async update({params}:HttpContextContract) {
-    return `PUT User: ${params.id}`
-  };
-
-  public async destroy({params}:HttpContextContract) {
-    return `DELETE User: ${params.id}`
-  };
-};
+  public async destroy({ params }: HttpContextContract) {
+    return `DELETE User: ${params.id}`;
+  }
+}
